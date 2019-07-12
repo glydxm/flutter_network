78a6aa9dd9d2751acd9167aaa5c25912d50b32f7
gdy <gaodayu@imdada.cn>
2019-07-12 13:53:30 +0800

    网络请求

--- lib/flutter_network.dart
+++ lib/flutter_network.dart
@@ -3,11 +3,32 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterNetwork {
+
   static const MethodChannel _channel =
       const MethodChannel('flutter_network');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  // get请求的封装，传入的两个参数分别是请求URL和请求参数，请求参数以map的形式传入，会在方法体中自动拼接到URL后面
+  static Future<String> get(String url, {Map<String, String> params}) async {
+    return await _getByHttp(url, params: params);
+  }
+
+  // post请求的封装，传入的两个参数分别是请求URL和请求参数，请求参数以map的形式传入
+  static Future<String> post(String url, {Map<String, String> params}) async {
+    return await _postByHttp(url, params: params);
+  }
+
+  //http网络请求get
+  static Future _getByHttp(String url, {Map<String, String> params}) async {
+    await _channel.invokeMethod("http_get");
+  }
+
+  //http网络请求post
+  static Future _postByHttp(String url, {Map<String, String> params}) async {
+    await _channel.invokeMethod("http_post");
   }
 }
