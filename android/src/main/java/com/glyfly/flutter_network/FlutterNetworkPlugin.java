0d20212c0209bc2898b0c52046ef9e884f1dc6a5
gdy <gaodayu@imdada.cn>
2019-07-12 13:44:23 +0800

    Initial commit

---
+++ android/src/main/java/com/glyfly/flutter_network/FlutterNetworkPlugin.java
@@ -0,0 +1,25 @@
+package com.glyfly.flutter_network;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** FlutterNetworkPlugin */
+public class FlutterNetworkPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_network");
+    channel.setMethodCallHandler(new FlutterNetworkPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
---
+++ ios/Classes/FlutterNetworkPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterNetworkPlugin.h"
+
+@implementation FlutterNetworkPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_network"
+            binaryMessenger:[registrar messenger]];
+  FlutterNetworkPlugin* instance = [[FlutterNetworkPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
