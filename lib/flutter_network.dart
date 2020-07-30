cdd4a2920a9e2ae6f92967f713f2eaca318f6095
gdy <gaodayu@imdada.cn>
2020-07-30 17:53:55 +0800

    header

--- lib/flutter_network.dart
+++ lib/flutter_network.dart
@@ -15,21 +15,21 @@ class FlutterNetwork {
   }
 
   // get请求的封装，传入的两个参数分别是请求URL和请求参数，请求参数以map的形式传入，会在方法体中自动拼接到URL后面
-  static Future<String> get(String url, {Map<String, String> params}) async {
-    return await _getByHttp(url, params: params);
+  static Future<String> get(String url, {Map<String, String> body, Map<String, Object> header}) async {
+    return await _getByHttp(url, params: body, header: header);
   }
 
   // post请求的封装，传入的两个参数分别是请求URL和请求参数，请求参数以map的形式传入
-  static Future<String> post(String url, {Map<String, String> params}) async {
-    return await _postByHttp(url, params: params);
+  static Future<String> post(String url, {Map<String, String> body, Map<String, Object> header}) async {
+    return await _postByHttp(url, body: body, header: header);
   }
 
   //http网络请求框架
-  static Future<String> _getByHttp(String url, {Map<String, String> params}) async {
-    if (params != null && params.isNotEmpty) {
+  static Future<String> _getByHttp(String url, {Map<String, String> body, Map<String, Object> header}) async {
+    if (body != null && body.isNotEmpty) {
       // 如果参数不为空，则将参数拼接到URL后面
       StringBuffer sb = StringBuffer("?");
-      params.forEach((key, value) {
+      body.forEach((key, value) {
         sb.write("$key" + "=" + "$value" + "&");
       });
       String paramStr = sb.toString();
@@ -37,10 +37,10 @@ class FlutterNetwork {
       url += paramStr;
     }
     try {
-      http.Response res = await http.get(url, headers: _getHeader());
+      http.Response res = await http.get(url, headers: _getHeader(header: header));
       Utf8Decoder utf8decoder = Utf8Decoder();
-      String body = utf8decoder.convert(res.bodyBytes);
-      return body;
+      String decodedBody = utf8decoder.convert(res.bodyBytes);
+      return decodedBody;
     } catch (error) {
       print(error);
       return Future.error(error);
@@ -48,12 +48,12 @@ class FlutterNetwork {
   }
 
   //http网络请求框架
-  static Future<String> _postByHttp(String url, {Map<String, String> params}) async {
+  static Future<String> _postByHttp(String url, {Map<String, String> body, Map<String, Object> header}) async {
     try {
-      http.Response res = await http.post(url, body: params, headers: _getHeader());
+      http.Response res = await http.post(url, body: body, headers: _getHeader(header: header));
       Utf8Decoder utf8decoder = Utf8Decoder();
-      String body = utf8decoder.convert(res.bodyBytes);
-      return body;
+      String decodedBody = utf8decoder.convert(res.bodyBytes);
+      return decodedBody;
     } catch (error) {
       print(error);
       return Future.error(error);
@@ -61,19 +61,12 @@ class FlutterNetwork {
   }
 
   //header生成
-  static Map<String, String> _getHeader() {
-    Map<String, String> header = Map();
-    header['is_flutter_osc'] = "1";
-    return header;
-  }
-
-  //http网络请求get
-  static Future _getByPlatformHttp(String url, {Map<String, String> params}) async {
-    return await _channel.invokeMethod("http_get", params);
-  }
-
-  //http网络请求post
-  static Future _postByPlatformHttp(String url, {Map<String, String> params}) async {
-    return await _channel.invokeMethod("http_post", params);
+  static Map<String, Object> _getHeader({Map<String, String> header}) {
+    Map<String, String> headers = Map();
+    if(header != null) {
+      headers.addAll(header);
+    }
+    headers['is_flutter_osc'] = "1";
+    return headers;
   }
 }
